fab62ef0458ad3baf7a324e8c961bcc507f6d170
Felipe Santos <felipecassiors@gmail.com>
2023-12-20 19:54:14 -0300

    Use file: npm import

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -43,7 +43,7 @@ const config = {
         ],
         tsconfig          : "../tsconfig.json",
         entryPointStrategy: "Expand",
-        plugin            : [ "../../typedoc-plugin-rename-defaults/index.js" ],
+        plugin            : [ "typedoc-plugin-rename-defaults" ],
         sidebar           : {
           position: 999,
         }
